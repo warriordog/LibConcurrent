ee136081951c5d591d66c90a3f702b8c45434a85
chris <acomputerdog@gmail.com>
2017-07-03 20:36:07 -0400

    Add existing code

---
+++ src/net/acomputerdog/concurrent/LockedNotifier.java
@@ -0,0 +1,57 @@
+package net.acomputerdog.concurrent;
+
+import java.util.concurrent.Semaphore;
+
+public class LockedNotifier {
+    private final Lock lock = new Lock(new Object());
+    private final Semaphore notifiedLock = new Semaphore(1);
+    private volatile int notifyCount = 0;
+
+    public boolean waitForNotify() {
+        return waitForNotify(-1);
+    }
+
+    public boolean waitForNotify(long timeout) {
+        // make sure there is not already a notification
+        notifiedLock.acquireUninterruptibly();
+        if (notifyCount > 0) {
+            // update atomically in case other thread is about to mark notified again
+            notifyCount--;
+            notifiedLock.release();
+
+            //System.out.println("Already notified");
+
+            return true;
+        }
+        notifiedLock.release();
+
+        // wait for notification
+        try {
+            // acquire lock
+            lock.lock(timeout);
+
+            // lock variable when we are notified
+            notifiedLock.acquireUninterruptibly();
+            try {
+                if (notifyCount > 0) {
+                    notifyCount--;
+                    return true;
+                } else {
+                    return false;
+                }
+            } finally {
+                notifiedLock.release();
+            }
+        } catch (InterruptedException e) {
+            return false;
+        }
+    }
+
+    public void release() {
+        notifiedLock.acquireUninterruptibly();
+        notifyCount++;
+        notifiedLock.release();
+
+        lock.unlockAll();
+    }
+}
